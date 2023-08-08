5496d975ccd9d44578cb57a356f669a81126cf86
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-08-08 22:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 // write js code here if required
+const timer = document.getElementById('timer')
+const date = new Date()
+timer.innerText = date.getDay() +"/"+ date.getMonth()+"/"+date.getFullYear()+","+ date.getHours()+":"+ date.getMinutes()+":"+ date.getSeconds()
